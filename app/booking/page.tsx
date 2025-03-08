82ffecd6dd85a37a22c184ee5e0a43d3a086645d
youssefhelmy1 <yousefhelmy399@gmail.com>
2025-03-08 07:07:52 +0300

    fix: Update date-fns imports to use named imports

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -1,12 +1,10 @@
 import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
-import format from 'date-fns/format'
-import parse from 'date-fns/parse'
-import startOfWeek from 'date-fns/startOfWeek'
-import getDay from 'date-fns/getDay'
+import { format, parse, startOfWeek, getDay } from 'date-fns'
+import { enUS } from 'date-fns/locale'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 
 const locales = {
-  'en-US': require('date-fns/locale/en-US'),
+  'en-US': enUS,
 }
 
 const localizer = dateFnsLocalizer({
